3ca494c0709e316aeba2caaf8bfbf025c028d5dc
Chris <chrispackett1998@gmail.com>
2018-07-31 14:16:09 -0400

    added computer mode.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,54 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const rockPaperScissorsDict = {
+  1: 'Rock',
+  2: 'Paper',
+  3: 'Scissors'
 }
 
-document.addEventListener('DOMContentLoaded', main)
+let randomGuess
+
+const randomComputerGuess = () => {
+  const randomNumber = Math.ceil(Math.random() * 3)
+  console.log(randomNumber)
+  randomGuess = rockPaperScissorsDict[randomNumber]
+  console.log(randomGuess)
+}
+
+const updatePlayerName = () => {
+  const playerName = document.querySelector('#player-name-input').value
+  console.log(playerName)
+  document.querySelector('#player-name-display').textContent = playerName
+}
+
+const compareForRock = () => {
+  compareWithComputer('Rock')
+}
+
+const compareForPaper = () => {
+  compareWithComputer('Paper')
+}
+
+const compareForScissors = () => {
+  compareWithComputer('Scissors')
+}
+
+const compareWithComputer = (gameAction) => {
+  if (gameAction === randomGuess) {
+    console.log('Go again')
+    randomComputerGuess()
+  }
+  else if ((gameAction === 'Rock' && randomGuess === 'Scissors') || (gameAction === 'Paper' && randomGuess === 'Rock') || (gameAction === 'Scissors' && randomGuess === 'Paper')) {
+    console.log('Good job')
+    document.querySelector('#output').textContent = 'Good job'
+  }
+  else {
+    console.log('You lose')
+    document.querySelector('#output').textContent = 'You lose'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', randomComputerGuess)
+document.querySelector('#player-name-input').onchange = updatePlayerName
+document.querySelector('#rock-button').addEventListener('click', compareForRock)
+document.querySelector('#paper-button').addEventListener('click', compareForPaper)
+document.querySelector('#scissors-button').addEventListener('click', compareForScissors)
+
